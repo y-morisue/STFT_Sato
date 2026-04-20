fb398e8c076843cf0a8cfb403540dd5e66348d0c
t-sato1111 <takato.s1111@icloud.com>
2026-04-20 23:53:39 +0900

    一連の動作を関数化しました
    subのほうが関数の中身です

---
+++ STFT_step5.m
@@ -0,0 +1,9 @@
+clear; close all; clc;
+
+% 引数の入力
+[audioSig, fs] = audioread('sig1.wav'); % wavファイル入力＆読み込み
+windowLength = 2 ^ 12; % 窓長
+shiftLength = windowLength / 2; % シフト長
+
+%関数の実行
+S = STFT_step5_sub(audioSig, fs, windowLength, shiftLength);
